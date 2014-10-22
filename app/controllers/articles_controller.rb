65486107a06d0db8716c33027a46bb05be88e16d
Andrew Songate <asongate@gmail.com>
2014-10-22 21:51:02 +0000

    update and delete an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,25 @@
         end
       end
    
+      def edit
+         @article = Article.find(params[:id])
+      end
+   
+   def update
+     @article = Article.find(params[:id])
+     if @article.update(article_params)
+       redirect_to @article
+     else
+       render "edit"
+     end
+   end
+   
+   def destroy
+     @article = Article.find(params[:id])
+     Article.destroy(@article)
+     redirect_to :action=> "index"
+   end
+   
      private
     def article_params
       params.require(:article).permit(:title, :content, :category_ids => [])
